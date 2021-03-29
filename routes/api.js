d0b4e1385e9e2c19ccbeeb8022080f820730f06a
Josua Wolber <josua.w@gmx.de>
2021-03-29 06:15:09 -0400

    Fix api/user/

--- routes/api.js
+++ routes/api.js
@@ -6,7 +6,7 @@ router.get('/', function (req, res, next) {
     res.render("apiUsage");
 });
 
-router.get('/:uid', function (req, res, next) {
+router.get('/user/:uid', function (req, res, next) {
     if (privileges.hasPrivilege(req.session.privs, privileges.Coworker) || req.session.uid == req.params.uid) {
         req.session.views++
         res.setHeader('Content-Type', 'text/html')
