99d9f672ecef6819e3229b179563b67d1a10b8c1
daWinzig <42668927+daWinzig@users.noreply.github.com>
2021-04-04 19:57:16 +0200

    added missing files from adding the menu

---
+++ public/javascripts/util/nav.js
@@ -0,0 +1,15 @@
+function addUnderline(obj) {
+    obj.children[0].style.textDecoration = "underline";
+}
+
+function removeUnderline(obj) {
+    obj.children[0].style.textDecoration = "";
+}
+
+function toggleControl() {
+    panel = document.getElementById("controlPanel");
+    if (panel.style.display === "block")
+        panel.style.display = "none";
+    else
+        panel.style.display = "block";
+}
