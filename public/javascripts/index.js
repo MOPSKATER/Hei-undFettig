e40fbade193c891c3bcb37e498734353f18fde88
Josua Wolber <josua.w@gmx.de>
2021-04-07 09:16:00 -0400

    Merged nav.js and index.js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -12,22 +12,6 @@ const testImg = {
 
 let counter = 0
 
-function addUnderline(obj) {
-    obj.children[0].style.textDecoration = "underline";
-}
-
-function removeUnderline(obj) {
-    obj.children[0].style.textDecoration = "";
-}
-
-function toggleControl() {
-    panel = document.getElementById("controlPanel");
-    if (panel.style.display === "block")
-        panel.style.display = "none";
-    else
-        panel.style.display = "block";
-}
-
 function displayLogin(name, points) {
     document.getElementById("summary").innerHTML = name + "<br>Treuepunkte: " + points;
     login = document.getElementById("login");
