61d1c997a289d32112e7c7095f798e104f13ce25
daWinzig <42668927+daWinzig@users.noreply.github.com>
2021-04-19 14:07:32 +0200

    fixed cart in accordance to feedback by Herr Kuti

--- public/javascripts/cart.js
+++ public/javascripts/cart.js
@@ -1,14 +1,21 @@
 
 // exampledata if cookies don't work (local)
 // TODO: change back to empty
-var cart = JSON.parse('[{"id":1.1,"name":"Salat","price":19.01,"count":4},{"id":1.2,"name":"Suppe","price":29.01,"count":1},{"id":2.1,"name":"Steak","price":19.02,"count":1},{"id":2.3,"name":"Schnitzel x2","price":39.00,"count":1},{"id":3.1,"name":"Salz","price":19.03,"count":3}]') // = [];
+var cart = [];
 var total = 0;
+var paypal = 0;
 
 function load() {
     update();
 
-    var today = new Date();
-    document.getElementById("time").value = ("0" + ((today.getHours() + Math.min(Math.round((today.getMinutes() + 15) / 60), 1))) % 24).slice(-2) + ":" + ("0" + (today.getMinutes() + 15) % 60).slice(-2);
+    var maxTime = 48;
+    for (var i = 1; i <= maxTime; i++) {
+        var opt = document.createElement("option");
+        var m = (i * 15) % 60;
+        var h = Math.floor((i * 15) / 60);
+        opt.text += ("0" + h).slice(-2) + ":" + ("0" + m).slice(-2);
+        document.getElementById("time").add(opt);
+    }
 }
 
 function update() {
@@ -32,15 +39,23 @@ function update() {
         })
     }
 
-    document.getElementById("total_price").innerHTML = "Preis gesammt: " + total.toFixed(2).replace(".",",") + "€";
+    paypal = 0;
+    if (document.getElementById("paypal").checked) paypal = (total * 0.0249 + 0.35);
+    document.getElementById("total_price").innerHTML = "Preis gesammt: " + (total + paypal).toFixed(2).replace(".",",") + "€";
     document.getElementById("cut_mwst").innerHTML = "Anteil MwSt: " + (total * 0.19).toFixed(2).replace(".",",") + "€";
-    document.getElementById("cut_paypal").innerHTML = "Paypal Gebühren: " + (total * 0.0249 + 0.35).toFixed(2).replace(".",",") + "€";
+    document.getElementById("cut_paypal").innerHTML = "Paypal Gebühren: " + paypal.toFixed(2).replace(".",",") + "€";
 }
 
 function changedCount(e) {
     var index = e.parentNode.parentNode.rowIndex;
     cart[index].count = e.value;
     setJSONCookie("cart", cart);
+
+    update();
+}
+
+function changedMethode() {
+    document.getElementById("cut_paypal").hidden = !document.getElementById("paypal").checked;
 
     update();
 }
@@ -55,13 +70,45 @@ function remove(e) {
 }
 
 function order() {
-    // temp until backend is implemented
     if (cart.length > 0) {
         if (confirm("Bestellung abschicken?")) {
+            var time = new Date();
+            time.setHours(time.getHours() + parseFloat(document.getElementById("time").value.split(":")[0]));
+            time.setMinutes(time.getMinutes() + parseFloat(document.getElementById("time").value.split(":")[1]));
+
+            var height = window.innerHeight;
+            let newWin = window.open("./bon.html", "bon", "toolbar=no,menubar=no,location=no,height=" + height + ",width=" + Math.floor(height*(7/10)));
+            newWin.onload = function() {
+                cart.forEach(function (item) {
+                    var newItem = document.getElementsByTagName("template")[0].content.cloneNode(true);
+                    newItem.querySelector(".num").innerHTML = item.id;
+                    newItem.querySelector(".name").innerHTML = item.name;
+                    newItem.querySelector(".price").innerHTML = item.price.toFixed(2).replace(".",",") + "€";
+                    newItem.querySelector(".count").innerHTML = "#" + item.count;
+                    newItem.querySelector(".remove").remove()
+                    newWin.document.getElementById("cartContent").append(newItem);
+                });
+                newWin.document.getElementById("retrieval").innerHTML = time;
+                newWin.document.getElementById("total").innerHTML = (total + paypal).toFixed(2).replace(".",",") + "€";
+                newWin.document.getElementById("cut_mwst").innerHTML = "Anteil MwSt: " + (total * 0.19).toFixed(2).replace(".",",") + "€";
+                if (document.getElementById("paypal").checked) {
+                    newWin.document.getElementById("cut_paypal").innerHTML = "Paypal Gebühren: " + paypal.toFixed(2).replace(".",",") + "€";
+                    newWin.document.getElementById("paym_methode").innerHTML = "PayPal";
+                }
+                if (document.getElementById("notice").value !== "") {
+                    newWin.document.getElementById("bon-notice").hidden = false;
+                    newWin.document.getElementById("bon-notice").innerHTML += document.getElementById("notice").value.replaceAll("\n","<br>");
+                }
+                delCookie("cart");
+                cart = [];
+                update();
+                window.location.href = "./order.html"; // = "../index.html"
+            }
+            // temp until backend is implemented
             var orders = getJSONCookie("orders") || [];
             var newOrder = {
                 name: "Peter Pan",
-                time: document.getElementById("time").value,
+                time: ["Mo","Di","Mi","Do","Fr","Sa","So"][time.getDay()-1] + " - " + time.getHours() + ":" + time.getMinutes(),
                 cart: cart,
                 notice: document.getElementById("notice").value,
                 total: total.toFixed(2),
@@ -69,9 +116,6 @@ function order() {
             };
             orders.push(newOrder);
             setJSONCookie("orders", orders);
-            delCookie("cart");
-            alert("Vielen Dank für Ihre Bestellung!\nTemporäre weiterleitung zu \"./order.html\" zu Demonstartionszwecken.")
-            window.location.href = "./order.html";
         }
     }
     else {
