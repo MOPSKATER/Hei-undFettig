b3c000c8db968f408e0b52520c8e9a36a02a83f9
Josua Wolber <josua.w@gmx.de>
2021-04-20 15:49:16 -0400

    Added pseudo user data response

--- routes/api.js
+++ routes/api.js
@@ -6,17 +6,19 @@ var router = express.Router();
 const path = require('path');
 
 router.get('/', function (req, res, next) {
-    //res.render("apiUsage");
     res.sendFile(path.join(__dirname + "/../views/apiUsage.html"))
 });
 
 router.get('/user/:uid', function (req, res, next) {
-    if (Privileges.hasPrivilege(req.session.privs, Privileges.Coworker) || req.session.uid == req.params.uid) {
-        res.setHeader('Content-Type', 'application/json')
-        res.write('<p>expires in: ' + (req.session.cookie.maxAge / 1000) + 's</p>')
-        res.end()
+    if (/*FIXME remove "!"*/!Privileges.hasPrivilege(req.session.privs, Privileges.Coworker) || req.session.uid == req.params.uid) {
+        res.setHeader('Content-Type', 'application/json');
+        res.write(JSON.stringify({
+            prename: "Peter", name: "Pan", points: 50, street: "Hafenstr. 49",
+            city: "Albcity", additionalAddress: "", postcode: "123456", accessLevel: Privileges.Guest
+        }))
+        res.end();
     } else { //Insufficient permissions
-        res.sendStatus(401)
+        res.sendStatus(401);
     }
 });
 
