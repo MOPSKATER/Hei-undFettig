a439adf61531eba8510061bb48eaa3e48c9517c2
dornbast <dornbast@hs-albsig.de>
2021-04-30 19:14:06 +0200

    add dropdown menu

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -30,3 +30,20 @@ function cycleStaff(dir) {
 
 window.onload = load;
 
+function mediaDropDown() {
+    document.getElementById("dropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("menuContainer");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
