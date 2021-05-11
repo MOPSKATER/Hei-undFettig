5e2f7b79933d2a3edc65379aa51d8281f7a2400f
dornbast <dornbast@hs-albsig.de>
2021-05-11 17:31:52 +0200

    fix owerflow function for controlIcon

--- public/javascripts/util/nav.js
+++ public/javascripts/util/nav.js
@@ -68,17 +68,5 @@ function displayLogout() {
 function mediaDropDown() {
     document.getElementById("dropdown").classList.toggle("show");
 }
+
   
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("menuContainer");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
