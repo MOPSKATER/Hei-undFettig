5f432c7c871ce4b93f7991ab651e43535adfe132
Josua Wolber <josua.w@gmx.de>
2021-05-12 12:41:07 -0400

    news

--- databasemanager.js
+++ databasemanager.js
@@ -11,6 +11,32 @@ const db = new sqlite3.Database("db.sqlite", (err) => {
 })
 
 const usedIDs = []
+var news = [
+    [
+        "Januar 2021: Frohes Neues!",
+        "Liebe Gäste, wir wünschen Ihnen und Ihrer Familie ein gutes und gesundes neues Jahr!"
+    ],
+    [
+        "Weihnachten 2020: Frohe Weihnachten!",
+        "Sie suchen für Ihre Lieben noch ein Weihnachtsgeschenk? Bei uns erhalten Sie Gutscheine (Betrag nach Wahl), Schnaps aus unserer historischen Schnapsbrennerei oder selbstgemachte Liköre. Schreiben Sie und eine E-Mail oder rufen Sie uns an."
+    ],
+    [
+        "Dezember 2020: Schließung",
+        "Liebe Gäste, unser Restaurant bleibt bis zum Ende des Lockdown geschlossen.<br>Wir wünschen Ihnen und Ihrer Familie ein schönes Weihnachtsfest und ein gutes, gesundes Jahr 2021.<br>Auf ein baldiges Wiedersehen"
+    ],
+    [
+        "Juli 2020: Sperrung Ortsduchfahrt",
+        "Liebe Gäste, die Ortsdurchfahrt von ist seit Juli gesperrt.<br>Wir möchten Sie darauf hinweisen, dass die Zufahrt trotzdem ohne Probleme möglich ist. Bitte folgen Sie hier der Umleitungs-Beschilderung. Wir hoffen, dass Sie trotz diesen Umständen den Weg zu uns finden.<br>Sollten Sie jedoch Hilfe benötigen können Sie uns gerne kontaktieren."
+    ],
+    [
+        "Mai 2020: Öffnung unter Verhaltensregeln",
+        "Liebe Gäste, wir öffnen unser Restaurant wieder ab dem 21.5.2020 für Sie!<br>Wir bitten Sie sich bei Ihrem Besuch an die Verhaltensregeln zu halten, die sie natürlich auch den ausgehängten Schildern entnehmen können. Bitte unterstützen Sie uns dabei, allen einen sicheren und schönen Besuch bei uns zu ermöglichen.<br>Es ergeben sich dadurch auch einige Neuerungen:<br><br>Geänderte Öffnungszeiten: wir sind nun Freitag von 15 – 21 Uhr und Samstag und Sonntag durchgehend (11- 21 Uhr) für Sie da. Die Küche ist jeweils durchgehend bis 20 Uhr geöffnet. Wir bitten Sie zu den Hauptgeschäftszeiten (Samstag und Sonntag 12 – 14 Uhr und 17 – 19 Uhr) einen Tisch zu reservieren.<br><br>Angepasste Speisekarte: Wir bieten in diesen Zeiten durchgehend warme Küche an. Unsere aktuelle Speisekarte finden Sie <a href=\"./menu.html\">hier</a>. Neu sind auch unsere Kaffeespezialitäten und selbstgemachten Kuchen.<br><br>Gerne können Sie unsere Gerichte auch abholen und bei Ihnen zu Hause genießen."
+    ],
+    [
+        "Wir freuen uns, Sie sobald wie möglich wieder im Heiß und Fettig begrüßen zu dürfen!",
+        ""
+    ]
+]
 
 db.run("CREATE TABLE IF NOT EXISTS users (uid text, prename text, name text, points integer, street text, number text, place text, plz integer, email text, salt text, password text, permissionlevel integer)", (err) => {
     if (err) {
@@ -38,7 +64,10 @@ db.run("CREATE TABLE IF NOT EXISTS users (uid text, prename text, name text, poi
 });
 db.run("CREATE TABLE IF NOT EXISTS cart (uid integer, itemid integer, amount integer)");
 db.run("CREATE TABLE IF NOT EXISTS item (id integer, name text, description text, price decimal)");
-db.run("CREATE TABLE IF NOT EXISTS news (id integer, caption text, text text, date date)");
+db.run("CREATE TABLE IF NOT EXISTS news (id integer, caption text, text text, date text)", (err) => {
+    for (i = 0; i < news.length; i++)
+        db.run("INSERT INTO news (id, caption, text, date) VALUES (?, ?, ?, null)", i, news[i][0], news[i][1])
+});
 db.run("CREATE TABLE IF NOT EXISTS orders (uid text, itemid integer, amount integer, datetime datetime)");
 
 const validKeys = ["prename", "name", "street", "number", "place", "plz", "email", "password"]
@@ -211,7 +240,7 @@ function IDGenerator() {
 module.exports = Databasemanager;
 const Accountmanager = require('./accountmanager')
 
-/*news = [
+news = [
     [
         "Januar 2021: Frohes Neues!",
         "Liebe Gäste, wir wünschen Ihnen und Ihrer Familie ein gutes und gesundes neues Jahr!"
@@ -236,4 +265,4 @@ const Accountmanager = require('./accountmanager')
         "Wir freuen uns, Sie sobald wie möglich wieder im Heiß und Fettig begrüßen zu dürfen!",
         ""
     ]
-]*/
+]
