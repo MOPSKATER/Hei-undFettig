a8c60723c0543920a6322485be2a686421a01462
Josua Wolber <josua.w@gmx.de>
2021-05-12 09:00:41 -0400

    Fixed patterns

--- ruleset.js
+++ ruleset.js
@@ -1,15 +1,15 @@
 const Ruleset = {
 
-    Name: { format: { pattern: "[a-zA-Z]" } },
+    Name: { format: { pattern: "[a-zA-Z]+" } },
     Uid: { presence: true, length: { is: 16 } },
     EMail: { email: true, presence: true },
     Changeemail: { email: true },
     Password: { presence: true, length: { is: 64 }, format: { pattern: "[0-9a-f]+" } },
     ChangePassword: { length: { is: 64 }, format: { pattern: "[0-9a-f]+" } },
-    Street: { format: { pattern: "[a-zA-Z]" } },
+    Street: { format: { pattern: "[a-zA-Z]+" } },
     Number: { numericality: true },
     Plz: { numericality: true },
-    Place: { format: { pattern: "[a-zA-Z]" } },
+    Place: { format: { pattern: "[a-zA-Z]+" } },
     Datetime: { presence: true, datetime: true }
 
 }
