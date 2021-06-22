6cb09fddb9513ce471399bc9ae7e95511a3077a5
Josua Wolber <josua.w@gmx.de>
2021-06-22 07:05:40 -0400

    Added Count rule

--- Backend/ruleset.js
+++ Backend/ruleset.js
@@ -9,12 +9,12 @@ const Ruleset = {
     ChangePassword: { length: { is: 64 }, format: { pattern: "[0-9a-f]+" } },
     Street: { format: { pattern: "[a-zA-Z]+" } },
     Number: { numericality: { onlyInteger: true, greaterThanOrEqual: 0, lessThan: 99999 } },
-    Plz: { numericality: { onlyInteger: true, greaterThanOrEqual: 9999, lessThan: 100000 } },
+    Plz: { numericality: { onlyInteger: true, greaterThanOrEqual: 10000, lessThan: 100000 } },
     Place: { format: { pattern: "[a-zA-Z]+" } },
     Datetime: { presence: true, format: { pattern: "[0-9]{4}\.[0-9]{2}\.[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}" } },
     Caption: { presence: true, format: { pattern: "[^<>]+" }, length: { maximum: 30 } },
-    Text: { format: { pattern: "[^<>]+" }, length: { maximum: 200 } }
-
+    Text: { format: { pattern: "[^<>]+" }, length: { maximum: 200 } },
+    Count: { presence: true, numericality: { onlyInteger: true, greaterThan: 0, lessThan: 100 } }
 }
 
 module.exports = Ruleset
