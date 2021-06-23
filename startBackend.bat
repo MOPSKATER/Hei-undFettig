baa65197253777adb48b7da7644f902facb81158
daWinzig <42668927+daWinzig@users.noreply.github.com>
2021-06-23 15:39:12 +0200

    start bats cls

--- startBackend.bat
+++ startBackend.bat
@@ -1,6 +1,10 @@
+@echo off
 
 @REM Frontend adress
 set FRONTEND="http://localhost:3000"
 
 cd Backend
+
+cls
+
 npm start %FRONTEND%
--- startFrontend.bat
+++ startFrontend.bat
@@ -1,6 +1,10 @@
+@echo off
 
 @REM Backend address
 set BACKEND="http://localhost:3001"
 
 cd Frontend
+
+cls
+
 npm start %BACKEND%
